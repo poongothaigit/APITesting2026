e5a6849746134441eb735db17830e8c799120cc5
poongothaigit <poongothai.chennappan@gmail.com>
2026-01-16 06:45:42 -0800

    Update API test validation-GET RESPONSE only

--- tests/Pet/get.spec.js
+++ tests/Pet/get.spec.js
@@ -4,11 +4,14 @@ import {logger} from "../../utils/logger";
 import petData from "../../testdata/petData.json";
 
 test("Get pet by ID", async({request})=>{
-    const response = await getPetById(161601);
+    const createResp = await createPet(petData);
+    const created = await createResp.json();
+    logger.info("Created pet response-----", {status:createResp.status()});
+    const response = await getPetById(created.id);
     logger.info("Get pet by ID response ----- ", {status:response.status()});
     expect(response.status()).toBe(200);
     
     const respBody = await response.json();
     console.log(respBody);
-    expect(respBody.id).toBe(petData.id);
+    expect(respBody.id).toBe(created.id);
 })
--- tests/Pet/post.spec.js
+++ tests/Pet/post.spec.js
@@ -1,16 +1,16 @@
-import {test, expect, request as playwrightRequest } from "@playwright/test";
-//import { request } from "node:http";
-import {createPet} from "../../services/petService";
-import {logger} from "../../utils/logger";
-import petData from "../../testdata/petData.json";
+// import {test, expect, request as playwrightRequest } from "@playwright/test";
+// //import { request } from "node:http";
+// import {createPet} from "../../services/petService";
+// import {logger} from "../../utils/logger";
+// import petData from "../../testdata/petData.json";
 
 
-test("create a pet", async({request})=>{
-    const response = await createPet(petData);
-    logger.info("Create Pet Response", {status: response.status()});
-    expect(response.status()).toBe(200);
-    const body = await response.json();
-    console.log(body);
-    expect(body.name).toBe(petData.name);
-     console.log("REsponse id----" + body.id);
-})
+// test("create a pet", async({request})=>{
+//     const response = await createPet(petData);
+//     logger.info("Create Pet Response", {status: response.status()});
+//     expect(response.status()).toBe(200);
+//     const body = await response.json();
+//     console.log(body);
+//     expect(body.name).toBe(petData.name);
+//      console.log("REsponse id----" + body.id);
+// })
